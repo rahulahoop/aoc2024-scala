c89af47fbfae08bd799997476f0ddc18c8c989d9
Rahul Bhonsale <satriales@rahuls-air.speedport.ip>
2024-12-03 10:01:11 +0100

    day three solution

---
+++ src/main/scala/DayThree.scala
@@ -0,0 +1,49 @@
+import scala.util.matching.Regex
+
+object DayThree extends App {
+
+  private val input = io.Source.fromResource("day_three.txt").mkString
+
+  val pattern: Regex = """mul\(\d{1,3},\d{1,3}\)""".r
+
+  def partOne(input: String): Int = {
+    pattern.findAllIn(input).foldLeft(0) { (acc, next) =>
+      acc + next.replace("mul(", "").replace(")", "").split(',').map(_.toInt).product
+    }
+  }
+
+  val adapted: Regex = """(mul\(\d{1,3},\d{1,3}\))|(don't\(\))|(do\(\))""".r
+
+  def partTwo(input: String) = {
+    val (result, _) = adapted.findAllIn(input).foldLeft((0, false)) { case ((acc, shouldIgnore), next) =>
+      next match
+        case "don't()" => (acc, true)
+        case "do()" => (acc, false)
+        case s if !shouldIgnore =>
+          (acc + s.replace("mul(", "").replace(")", "").split(',').map(_.toInt).product, shouldIgnore)
+        case _ => (acc, shouldIgnore)
+    }
+
+    result
+  }
+
+  println(s"the result for partOne is ${partOne(input)}")
+  println(s"the result for partTwo is ${partTwo(input)}")
+
+  /* Test */
+
+  val testInput = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+
+  def testPartOne() = {
+    assert(partOne(testInput) == 161)
+  }
+
+  val testInputTwo = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+
+  def testPartTwo() = {
+    assert(partTwo(testInputTwo) == 48)
+  }
+
+  testPartOne()
+  testPartTwo()
+}
